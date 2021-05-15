47abb1308a4eeb379cd4ed4f1cdcd4461645ab60
take1092 <take-c@ff.iij4u.or.jp>
2021-05-15 10:26:16 +0900

    first commit

---
+++ zenhan.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <imm.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    const LPARAM IMC_GETOPENSTATUS = 5;
+	const LPARAM IMC_SETOPENSTATUS = 6;
+
+	HWND hwnd = GetForegroundWindow();
+	if(hwnd == NULL) return 0;
+
+	HWND ime =  ImmGetDefaultIMEWnd(hwnd);
+	if(ime == NULL) return 0;
+
+	LRESULT stat;
+	if(argc < 2){
+		stat = SendMessage(ime, WM_IME_CONTROL, IMC_GETOPENSTATUS, 0);
+	}
+	else{
+		stat = (LPARAM)atoi(argv[1]);
+		SendMessage(ime, WM_IME_CONTROL, IMC_SETOPENSTATUS, stat);
+	}
+	printf("%lld\n", stat);
+	return 0;
+}
